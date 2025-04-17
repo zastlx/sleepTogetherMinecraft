5e92a867ad437e6a765cc95ab05346b34604c44d
zastlx <zastix@zastix.club>
2025-04-17 18:54:56 -0500

    remove useless newline

--- src/main/java/club/zastix/sleeptogether/Sleeptogether.java
+++ src/main/java/club/zastix/sleeptogether/Sleeptogether.java
@@ -3,7 +3,6 @@ package club.zastix.sleeptogether;
 import net.fabricmc.api.ModInitializer;
 
 public class Sleeptogether implements ModInitializer {
-
     @Override
     public void onInitialize() {
     }
